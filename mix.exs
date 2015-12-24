16309066ad1e6395315f4350f01c2c4792d37b6f
Michael Truog <mjtruog@gmail.com>
2015-12-23 19:44:28 -0800

    Version 1.5.1.

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule CloudIServiceMonitoring do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_monitoring,
+     version: "1.5.1",
+     language: :erlang,
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:exometer,
+      [git: "https://github.com/Feuerlabs/exometer.git",
+       tag: "1.2.1"]},
+     {:folsom, "~> 0.8.3"},
+     {:cloudi_core, "~> 1.5.1"},
+     {:key2value, "~> 1.5.1"}]
+  end
+
+  defp description do
+    "CloudI Monitoring Service"
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown),
+     maintainers: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_monitoring"}]
+   end
+end
