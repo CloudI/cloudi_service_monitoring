0d48f6c2d02ed6e96afdc4e48fd0bf001ca274b7
Michael Truog <mjtruog@gmail.com>
2017-06-07 13:00:40 -0700

    Version 1.7.1.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceMonitoring do
 
   def project do
     [app: :cloudi_service_monitoring,
-     version: "1.7.0",
+     version: "1.7.1",
      language: :erlang,
      description: description(),
      package: package(),
@@ -18,8 +18,8 @@ defmodule CloudIServiceMonitoring do
       [git: "https://github.com/Feuerlabs/exometer.git",
        ref: "7a7bd8d2b52de4d90f65aa3f6044b0e988319b9e"]},
      {:folsom, "~> 0.8.3"},
-     {:cloudi_core, "~> 1.7.0"},
-     {:key2value, "~> 1.7.0"}]
+     {:cloudi_core, "~> 1.7.1"},
+     {:key2value, "~> 1.7.1"}]
   end
 
   defp description do
