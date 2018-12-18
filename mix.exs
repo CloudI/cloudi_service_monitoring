57ee5b5590917237d277746c2239e49fcf660227
Michael Truog <mjtruog@protonmail.com>
2018-12-17 21:44:32 -0800

    Version 1.7.5.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceMonitoring do
 
   def project do
     [app: :cloudi_service_monitoring,
-     version: "1.7.4",
+     version: "1.7.5",
      language: :erlang,
      erlc_options: [
        :debug_info,
@@ -31,8 +31,8 @@ defmodule CloudIServiceMonitoring do
   defp deps do
     [{:exometer_core, "~> 1.5.2"},
      {:folsom, "~> 0.8.5"},
-     {:cloudi_core, "~> 1.7.4"},
-     {:key2value, "~> 1.7.4"}]
+     {:cloudi_core, "~> 1.7.5"},
+     {:key2value, "~> 1.7.5"}]
   end
 
   defp description do
